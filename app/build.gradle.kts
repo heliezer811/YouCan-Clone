e4b26dae7ef4dd5ea844a07803982b81c41c8783
Heliézer <59342457+heliezer811@users.noreply.github.com>
2026-09-01 14:46:52 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,15 @@ android {
 }
 
 dependencies {
+    // Dependências anteriores...
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    
+    // Room Database
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+    // Use ksp ou kapt na prática para o compiler do Room
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
